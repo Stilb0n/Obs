ac953d283e48cc469f7bef98091af57027dc300c
Stilb0n <stilbon01@gmail.com>
2023-03-10 20:32:50 +0700

    Initial commit

---
+++ Obs/Obs.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>#include <fstream>     
+ using namespace std;
+#include "State.cpp"
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	FileEditor a("C:\\101\\infile.txt");
+	string b;
+	cin >> b;	
+	a.write(b);
+}	
---
+++ Obs/State.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <fstream> 
+using namespace std; 
+
+class FileEditor {
+bool existing = 1;
+int size = 0;
+fstream file;
+bool edited;
+  
+ public:
+
+	 FileEditor(const char* path)
+ {
+	 file.open(path);
+	 if (file.bad() == true)
+		 existing = 0;
+	 else
+	 {
+		 file.seekg(0, std::ios::end);
+		 size = file.tellg();
+		 cout << size;
+	 }
+	 file.seekg(0, std::ios::beg);
+ }
+
+
+	
+	 void write(string data) {
+		 if (existing)
+		 {
+			 file << data;
+			 edited = 1;
+			 file.seekg(0, std::ios::end);
+			 size = file.tellg();
+			 cout << size;
+		 }
+	 }
+
+	
+	
+};	
